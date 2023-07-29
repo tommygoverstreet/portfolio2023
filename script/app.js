6b87356cd431fb06f0e21ee125587a966de5d911
Texoma2022 <TOMMYGOVERSTREET@GMAIL.COM>
2023-07-29 05:10:04 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-// Assign Symbols to Query Selectors
+// Assign Symbols to Query Selectors// Assign Symbols to Query Selectors
 const $ = document.querySelector.bind(document);
 
 const $$ = document.querySelectorAll.bind(document);
@@ -8,4 +8,19 @@ const toggleMenu = () => {
   $("#navIcon1").classList.toggle("hide");
   $("#navIcon2").classList.toggle("hide");
 };
-    
+
+var acc = $$(".accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "grid") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "grid";
+    }
+  });
+}
